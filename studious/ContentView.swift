3c047a59a3a81629a61c26cece114d7b7da3403a
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-07-19 17:17:57 -0400

    Initial Commit

---
+++ studious/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  studious
+//
+//  Created by Vinh Le on 7/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ studious/studiousApp.swift
@@ -0,0 +1,17 @@
+//
+//  studiousApp.swift
+//  studious
+//
+//  Created by Vinh Le on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct studiousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
